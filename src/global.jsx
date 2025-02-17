7e702c60d56c1d7cbb267795942734d17f46f818
Criminalist <vrazlada@mail.ru>
2025-02-17 08:30:32 +0300

    feat: change minista config

--- src/global.jsx
+++ src/global.jsx
@@ -10,15 +10,16 @@ export default function (props) {
     const {children, title, url, isHeaderFixed} = props;
     return (
         <>
-            <Head htmlAttributes={{lang: "en"}}>
-                <title>Stream Vibe | {title}</title>
-                <script src='/src/main.js' type='module'/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-            </Head>
-            <Header url={url} isFixed={isHeaderFixed}/>
+          <Head htmlAttributes={{lang: "en"}}>
+            <title>Stream Vibe | {title}</title>
+            <script src='/src/main.js' type='module'/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <base href="/stream-vibe/"/>
+          </Head>
+          <Header url={url} isFixed={isHeaderFixed}/>
             <Content isResetPaddingTop={isHeaderFixed}>
               {children}
               <Banner/>
